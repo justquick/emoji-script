11d306ff4972260c6e9d1e4c5600e08363b5f877
Justin Quick <justquick@gmail.com>
2014-05-18 22:41:28 -0400

    test compat

--- emoji_script/tests/samples/14.py
+++ emoji_script/tests/samples/14.py
@@ -6,9 +6,9 @@ def median(pool):
     copy = sorted(pool)
     size = len(copy)
     if size % 2 == 1:
-        return copy[(size - 1) / 2]
+        return copy[int((size - 1) / 2)]
     else:
-        return (copy[size/2 - 1] + copy[size/2]) / 2
+        return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
